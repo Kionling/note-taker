b0a5f70ea8b448e74699340cfa2ab49c9823da6c
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-19 13:59:13 -0700

    Creating psuedocode for apiRoutes script

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,15 @@
 var path = require("path");
 const fs = require("fs");
+
+//need a retrival function to retrive code from db.json file
+
+// need a fucntion that adds notes
+
+
+/// function for deleting notes
+
+
+///save notes functions
+
+
+//method for exporting api information 
