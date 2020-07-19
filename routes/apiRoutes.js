ebe41403d80efaccb080122509bf7e58b968a538
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-19 14:16:42 -0700

    creating a variable that reads the db.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,9 @@ var path = require("path");
 const fs = require("fs");
 
 //need a retrival function to retrive code from db.json file
+function getDbNotes () {
+    var notes = fs.readFile(path.join(__dirname, "../db/db.json", "utf8"))
+}
 
 // need a fucntion that adds notes
 
